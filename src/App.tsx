528c58af738c400ba95765210e32ba8ad97a8c61
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-04-16 13:53:14 -0300

    Finalize an example code for demonstrating error handling 💞

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+
+window.addEventListener("unhandledrejection", (event) => {
+  console.error("Unhandled promise rejection:", event.reason);
+});
+
+class ErrorBoundary extends React.Component<
+  {
+    children: React.ReactNode;
+  },
+  {
+    hasError: boolean;
+  }
+> {
+  constructor(props: { children: React.ReactNode }) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <>
+          <h1>Something went wrong.</h1>
+          <button
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Reload the page
+          </button>
+        </>
+      );
+    }
+
+    return this.props.children;
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  if (count === 3) {
+    throw new Error("Something went wrong");
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      throw new Error("Something went wrong inside effect");
+    }, 1000);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Counter</h1>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button
+        onClick={() => {
+          throw new Error("Something went wrong inside handler");
+        }}
+      >
+        Throw error inside handler
+      </button>
+
+      <button
+        onClick={() => {
+          Promise.reject("Something went wrong inside promise");
+        }}
+      >
+        Promise rejection inside handler
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default function AppWithErrorBoundary() {
+  return (
+    <main>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </main>
+  );
+}
